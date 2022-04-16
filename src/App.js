1b0eb57d0ab3e2ca6b8fe1b720c7d53a97ac01cf
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-04-16 16:23:20 +0600

    components created successfully

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import SignUp from "./components/SignUp/SignUp";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/signup" element={<SignUp></SignUp>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Login";
+
+const Login = () => {
+  return (
+    <div className="form-container">
+      <h1>This is login page</h1>
+    </div>
+  );
+};
+
+export default Login;
