9e2a25e5b901829250b53e53c65a70741b62b6d7
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2022-04-23 23:43:42 +0600

    updated successfully

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./NotFound.css";
-
+// This is not found / Error page
 const NotFound = () => {
   return (
     <div>
       <h1>This is NotFound page</h1>
+      <h3>Error 404 !!!</h3>
     </div>
   );
 };
